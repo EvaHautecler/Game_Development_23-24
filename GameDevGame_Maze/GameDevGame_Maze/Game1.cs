44d5ae0515114844f1ee4ea6f1c1d9ee2e1bf296
EvaHautecler <eva.hautecler@outlook.com>
2023-11-02 13:50:27 +0100

    Achtergrond toegevoegd

---
+++ GameDevGame_Maze/GameDevGame_Maze/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameDevGame_Maze
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D backgroundTexture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            backgroundTexture = Content.Load<Texture2D>("floor");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, 800, 480), Color.White);
+            _spriteBatch.End();
+
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameDevGame_Maze/GameDevGame_Maze/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new GameDevGame_Maze.Game1();
+game.Run();
