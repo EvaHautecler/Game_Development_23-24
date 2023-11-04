aab080550363f2c270f55e8ec98602e5d3d1fef1
EvaHautecler <eva.hautecler@outlook.com>
2023-11-04 13:36:01 +0100

    Animation klassen

---
+++ GameDevGame_Maze/GameDevGame_Maze/Animaties/AnimationFrame.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevGame_Maze.Animaties
+{
+    public class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+        public AnimationFrame(Rectangle sourceRectangle)
+        {
+            SourceRectangle = sourceRectangle;
+        }
+    }
+}
--- GameDevGame_Maze/GameDevGame_Maze/Hero.cs
+++ GameDevGame_Maze/GameDevGame_Maze/Hero.cs
@@ -1,4 +1,5 @@
-﻿using GameDevGame_Maze.Interfaces;
+﻿using GameDevGame_Maze.Animaties;
+using GameDevGame_Maze.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -15,25 +16,36 @@ namespace GameDevGame_Maze
         private Rectangle heroRectangle;
         private int schuifOp_X = 0;
 
+        Animation animation;
+
         public Hero(Texture2D texture)
         {
             textureHero = texture;
-            heroRectangle = new Rectangle(schuifOp_X, 0, 96, 96);
+            //heroRectangle = new Rectangle(schuifOp_X, 0, 96, 96);
+            animation = new Animation();
+            animation.AddFrame(new AnimationFrame(new Rectangle(0, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(96, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(192, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(288, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(384, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(480, 0, 96, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(576, 0, 96, 96)));
         }
 
 
         public void Update()
         {
-            schuifOp_X += 96;
+            /*schuifOp_X += 96;
             if (schuifOp_X > 576)
             {
                 schuifOp_X = 0;
             }
-            heroRectangle.X = schuifOp_X;
+            heroRectangle.X = schuifOp_X;*/
+            animation.Update();
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(textureHero, new Vector2(0, 0), heroRectangle, Color.White);
+            spriteBatch.Draw(textureHero, new Vector2(0, 0), animation.CurrentFrame.SourceRectangle, Color.White);
         }
     }
     
