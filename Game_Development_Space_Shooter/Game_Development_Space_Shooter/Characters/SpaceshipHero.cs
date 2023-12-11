357500473d1d05cae2458e6c9ce43118e5d103b4
EvaHautecler <eva.hautecler@outlook.com>
2023-12-11 17:03:58 +0100

    draaien spaceship toevoegen aan KeyboardReader

--- Game_Development_Space_Shooter/Game_Development_Space_Shooter/Characters/SpaceshipHero.cs
+++ Game_Development_Space_Shooter/Game_Development_Space_Shooter/Characters/SpaceshipHero.cs
@@ -1,4 +1,5 @@
-﻿using Game_Development_Space_Shooter.Input;
+﻿using Game_Development_Space_Shooter.Animaties;
+using Game_Development_Space_Shooter.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -15,6 +16,7 @@ namespace Game_Development_Space_Shooter.Characters
         private Rectangle spaceshipRectangle;
         private float airplaneSpeed = 2.0f;
         private KeyboardReader keyboardReader;
+        private Animation animationSpaceship;
 
         public SpaceshipHero(Texture2D spaceshipTexture, KeyboardReader keyboardReader)
         {
@@ -22,17 +24,21 @@ namespace Game_Development_Space_Shooter.Characters
             this.keyboardReader = keyboardReader;
 
             spaceshipRectangle = new Rectangle(500, 270, 76, 48);
+
+            animationSpaceship = new Animation();
+            animationSpaceship.GetFramesFromTextureProperties(spaceshipTexture.Width, spaceshipTexture.Height, 1, 1);
             keyboardReader = new KeyboardReader();
         }
 
         public void Update(GameTime gameTime)
         {
             spaceshipRectangle = keyboardReader.ReadInput(spaceshipRectangle, gameTime);
+            animationSpaceship.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(spaceshipTexture, spaceshipRectangle, Color.White);
+            spriteBatch.Draw(spaceshipTexture, spaceshipRectangle,animationSpaceship.CurrentFrame.SourceRectangle, Color.White, keyboardReader.AngleTotation(), new Vector2(spaceshipTexture.Width/2, spaceshipTexture.Height/2), SpriteEffects.None, 0f);
         }
     }
 }
--- Game_Development_Space_Shooter/Game_Development_Space_Shooter/Input/KeyboardReader.cs
+++ Game_Development_Space_Shooter/Game_Development_Space_Shooter/Input/KeyboardReader.cs
@@ -13,6 +13,40 @@ namespace Game_Development_Space_Shooter.Input
         private Rectangle spaceshipRectangle;
         private float spaceshipSpeed = 2.0f;
 
+        public float AngleTotation()
+        {
+            float angle = 0f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                angle += MathHelper.ToRadians(90f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                angle += MathHelper.ToRadians(180f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                angle += MathHelper.ToRadians(270f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                angle -= MathHelper.ToRadians(45f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                angle += MathHelper.ToRadians(315f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                angle -= MathHelper.ToRadians(135f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                angle -= MathHelper.ToRadians(225f);
+            }
+            return angle;
+        }
+
         public Rectangle ReadInput(Rectangle spaceshipRectangle, GameTime gameTime)
         {
             this.spaceshipRectangle = spaceshipRectangle;
