fd18ce4b4d040c05656968334bf1ee5fdb105638
EvaHautecler <eva.hautecler@outlook.com>
2023-12-19 18:41:05 +0100

    changing background

--- Game_Development_Space_Shooter/Game_Development_Space_Shooter/Game1.cs
+++ Game_Development_Space_Shooter/Game_Development_Space_Shooter/Game1.cs
@@ -63,7 +63,7 @@ namespace Game_Development_Space_Shooter
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            backgroundTexture = Content.Load<Texture2D>("space");
+            backgroundTexture = Content.Load<Texture2D>("SpaceBackground");
             spaceshipTexture = Content.Load<Texture2D>("Move1");
             laserTexture = Content.Load<Texture2D>("Charge");
             enemySpaceshipTexture = Content.Load<Texture2D>("enemySpaceship");
